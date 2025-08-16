9f5871c8e3e03f718a228d793a3947691e6ebcfc
Ethan Soh <ethan.sohyt@gmail.com>
2025-08-16 10:11:28 +0800

    StarGameView

---
+++ Challenge 2/startGame.swift
@@ -0,0 +1,18 @@
+//
+//  startGame.swift
+//  Challenge 2
+//
+//  Created by Ethan Soh on 16/8/25.
+//
+
+import SwiftUI
+
+struct startGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    startGame()
+}
