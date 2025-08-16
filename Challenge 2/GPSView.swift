2f39d13d43b005bdf0a2080614378ec0c059a5f7
Ethan Soh <ethan.sohyt@gmail.com>
2025-08-16 10:53:26 +0800

    StartGame with timer

---
+++ Challenge 2/GPSView.swift
@@ -0,0 +1,18 @@
+//
+//  GPSView.swift
+//  Challenge 2
+//
+//  Created by Ethan Soh on 16/8/25.
+//
+
+import SwiftUI
+
+struct GPSView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GPSView()
+}
--- Challenge 2/startGame.swift
+++ Challenge 2/startGame.swift
@@ -6,13 +6,45 @@
 //
 
 import SwiftUI
-
+import MapKit
 struct startGame: View {
+    @State private var currentDate = Date.now
+    @State private var elapsedTime: Int = 0
+    @State private var timer: Timer? = nil
+    var formattedTime: String {
+           let minutes = elapsedTime / 60
+           let seconds = elapsedTime % 60
+           return String(format: "%02d:%02d", minutes, seconds)
+       }
+       
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Today's Location is...")
+                .bold()
+                .font(.largeTitle)
+                .padding()
+    
+            Image("Image 1")
+                .resizable()
+                .padding()
+            Text("Time: \(formattedTime)")
+                .bold()
+                .monospaced()
+                .font(.largeTitle)
+                .onAppear {
+                    elapsedTime = 0
+                    timer?.invalidate()
+                    timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                        elapsedTime += 1
+                    }
+                    
+                    
+                }
+            
+            
+        }
     }
 }
-
-#Preview {
-    startGame()
-}
+    #Preview {
+        startGame()
+    }
