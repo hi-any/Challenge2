893d5ad26b75596ee178978fe008917101206912
Yihan <yangyihan2023@gmail.com>
2025-08-16 09:39:32 +0800

    Initial Commit

---
+++ Challenge 2/Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2App.swift
+//  Challenge 2
+//
+//  Created by Yang Yihan on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
